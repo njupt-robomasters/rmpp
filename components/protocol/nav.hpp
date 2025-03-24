4741c39f5ee27e8609dea89979df2149220696a9
rock3t <2352089873@qq.com>
2025-03-24 22:18:09 +0800

    nav组件编写完毕

--- components/protocol/nav.hpp
+++ components/protocol/nav.hpp
@@ -3,10 +3,12 @@
 
 class NAV {
 public:
+
+    float position_x=0 ,position_y=0 ,position_yaw=0;//当前车的位置
+    float vel_x=0,vel_y=0,vel_yaw=0;//目标速度
+
     void SendTargetData( float x,  float y,  float yaw);
-    void SendRefereeData(bool team_is_red, float bullet_speed);
     void ParseStreamingData(const uint8_t *data, uint32_t len);
 
-private:
 };
 
