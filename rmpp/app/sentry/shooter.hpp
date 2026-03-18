1fb47ebb749a2cb0d5a0c1625ccbf8f3f87d396d
xzqiaochu <xzzhangqiaochu@163.com>
2026-03-18 15:36:08 +0800

    fix(gimbal): 修正云台偏移角度和限位参数
    - 将偏移角度从 137.1 度调整为 -72 度
    - 将最小限位角度从 -25 度调整为 -22 度
    
    fix(shooter): 修正摩擦轮电机ID配置
    
    - 将 rub1 电机的 master_id 和 slave_id 从 0x20 调整为 0x21
    - 将 rub2 电机的 master_id 和 slave_id 从 0x21 调整为 0x22

--- rmpp/app/sentry/shooter.hpp
+++ rmpp/app/sentry/shooter.hpp
@@ -11,15 +11,15 @@
 // 摩擦轮
 inline VESC rub1({
                      .can_port = 2,
-                     .master_id = 0x20,
-                     .slave_id = 0x20,
+                     .master_id = 0x21,
+                     .slave_id = 0x21,
                  },
                  {});
 
 inline VESC rub2({
                      .can_port = 2,
-                     .master_id = 0x21,
-                     .slave_id = 0x21,
+                     .master_id = 0x22,
+                     .slave_id = 0x22,
                  },
                  {});
 
